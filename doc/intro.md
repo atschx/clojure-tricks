f8b6cc75f6d3f34ab78b86626dd8f57470090f8c
Albert <atschx@gmail.com>
2016-03-01 17:11:16 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-tricks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_tricks/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-tricks.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-tricks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
